4b4c829a5be3b9e023cafa26bdbe4e7dc2765336
Aziz Abduhakimov <byfuzayliy@gmail.com>
2025-06-05 14:29:57 +0500

    Added footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.scss";
+import { FaTelegram, FaYoutube, FaLinkedin, FaGithub } from "react-icons/fa";
 
+import logo from "../../assets/images/download.svg";
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="footer container">
+        <div>
+          <Link to="/" className="footer-logo">
+            <img src={logo} alt="" />
+            <h2>Fuzayliy</h2>
+          </Link>
+        </div>
+        <div className="social-icons">
+          <a
+            href="https://t.me/byfuzayliy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegram />
+          </a>
+          <a
+            href="https://youtube.com/byfuzayliy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube />
+          </a>
+          <a
+            href="https://linkedin.com/in/byfuzayliy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/byfuzayliy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+      <div className="footer-top">
+        © 2025 Aziz Abduhakimov. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
